ebd351c9d82590f6f579e6797aac4a1f39391401
Blake Montgomery <blakemontgomery312@gmail.com>
2022-01-16 12:58:12 -0600

    Organize Server.js

--- server.js
+++ server.js
@@ -1,41 +1,47 @@
-// DEPENDENCIES
-const express = require('express');
-const app = express();
-require('dotenv').config();
+// ==================================================
+// ============  DEPENDENCIES  ======================
+// ==================================================
 const methodOverride = require('method-override');
 const session = require('express-session');
+const express = require('express');
 
+// Require Models
 const Product = require('./models/product');
 
-// Environment Variables
+// Import Environment Variables
+require('dotenv').config();
 const PORT = process.env.PORT;
 const DATABASE_URL = process.env.DATABASE_URL;
+const SESSION_SECRET = process.env.SECRET;
+
+// Instantiate App
+const app = express();
 
 
-// Database Connection
+// ==================================================
+// ============  Database Connection  ===============
+// ==================================================
 const mongoose = require('mongoose');
-
 mongoose.connect(DATABASE_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
 });
 
-
-// Database Connection Error/Success
-// Define callback function for various events
 const db = mongoose.connection;
 db.on('error', (error)=>console.log(error.message + 'Is Mongo not working?'));
 db.on('connected', ()=>console.log('MongoDB is connected'));
 db.on('disconnected', ()=>console.log('MongoDB has been disconnected'));
 
 
-// MIDDLEWARE
+// ==================================================
+// =============  MIDDLEWARE  =======================
+// ==================================================
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(
     session({
-      secret: process.env.SECRET,
+      secret: SESSION_SECRET,
       resave: false,
       saveUninitialized: false
     })
@@ -43,14 +49,15 @@ app.use(
 
 
 // ==================================================
-// ROUTES/CONTROLLERS
+// =========  ROUTES/CONTROLLERS  ===================
 // ==================================================
-// ROOT
+// ROOT (Landing Page)
 app.get('/', (req,res)=>{
     res.locals.currentUser = req.session.currentUser;
     res.render('landing.ejs');
 });
 
+// SPACEBATTLE GAME
 app.get('/spacebattle', (req,res)=>{
     res.render('spacebattle/spacebattle.ejs');
 })
@@ -69,7 +76,7 @@ app.use('/sessions', sessionsController);
 
 
 // ==================================================
-//  SERVER INSTANCE
+// =============  SERVER INSTANCE  ==================
 // ==================================================
 app.listen(PORT, ()=>{
     console.log(`Server listening on port ${PORT}`);
