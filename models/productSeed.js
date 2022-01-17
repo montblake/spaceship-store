43a68d0575ba569c27903dddf11c4057820ffb4e
Blake Montgomery <blakemontgomery312@gmail.com>
2022-01-17 13:05:18 -0600

    Update Product Seed Data

--- models/productSeed.js
+++ models/productSeed.js
@@ -1,151 +1,151 @@
 module.exports = [
     {
-        name: 'ASS2000',
+        name: 'Battle Station',
         description: "Quisque tincidunt leo non ex faucibus interdum eu at sapien. Sed tincidunt sit amet enim ac commodo. Quisque ac placerat eros. Aenean facilisis pulvinar lectus ut ultrices. Praesent augue lectus, porta at ultricies vitae, hendrerit sed diam. Nunc vitae dolor ut libero ullamcorper bibendum. In euismod at purus eu aliquet. Sed et nisi malesuada, aliquet tortor eget, convallis lectus. Nullam purus tortor, ornare vel urna semper, commodo cursus lorem. In efficitur nulla sapien. Phasellus nunc elit, sollicitudin nec pulvinar at, cursus in est. Mauris ante dui, tincidunt eget viverra eget, venenatis quis tortor. Fusce non diam ornare, consectetur metus eget, efficitur felis. Ut tristique metus id nisl ornare, et cursus magna scelerisque. Curabitur vestibulum orci vel venenatis congue. Nunc porttitor risus mauris, sit amet elementum massa aliquet ac.",
         img: '/images/alien_slave_ship.webp',
-        price: 5000,
+        price: 5000000,
         qty: 6
     },
     {
         name: 'Ares Lunar Lander',
         description: "Aenean a dictum nibh. Sed aliquam augue sit amet nibh interdum feugiat. Integer ac nulla a metus lobortis molestie. Sed non magna nec magna lacinia fermentum. Proin congue, sapien et volutpat bibendum, nisi quam vestibulum velit, ac lobortis ante libero non metus. Fusce imperdiet ullamcorper condimentum. Nulla a tristique sem. Cras in lorem eleifend, iaculis enim ut, porta dolor. Vivamus sit amet mattis eros. Nam condimentum, tortor at placerat tempus, urna leo fermentum felis, ut posuere eros nulla eu lacus. Nulla cursus faucibus eleifend.",
         img: '/images/ares_lunar_lander.webp',
-        price: 120,
+        price: 125000,
         qty: 12
     },
     {
         name: 'Battlestar',
         description: "Mauris et turpis ut erat consequat auctor at sit amet velit. Etiam nec varius neque. Vestibulum malesuada mauris eros, nec rutrum diam condimentum quis. Donec porta nisi sed nulla imperdiet pretium. Proin feugiat a magna vitae maximus. Morbi eget feugiat tellus, ut lacinia erat. Cras ut ante non tellus blandit consectetur. Cras sed tincidunt massa, eu rutrum nulla. Phasellus laoreet diam posuere eros sollicitudin tempor. Donec sit amet mollis odio. Quisque egestas, dolor vitae bibendum aliquet, mi lacus egestas risus, in accumsan justo tellus in felis.",
         img: '/images/battlestar.webp',
-        price: 120,
-        qty: 12
+        price: 6200000,
+        qty: 4
     },
     {
         name: 'Colonial Shuttle',
         description: "Etiam bibendum mollis dui, vitae suscipit enim semper eu. Vivamus vitae dapibus purus. Morbi consectetur eget eros at consequat. Etiam aliquet elit sed risus egestas, ac tempor dui bibendum. Nullam feugiat nibh quis nulla cursus, ac commodo nunc suscipit. Suspendisse luctus mattis mi non fringilla. Maecenas lacinia convallis tempor. Suspendisse lacinia purus erat, id fringilla ante eleifend vel. Pellentesque ut lorem sed ligula gravida cursus id posuere mi. Quisque finibus nulla augue. Donec a faucibus risus. Donec in fermentum purus. Phasellus fringilla ante in sapien mollis fringilla. Aliquam erat volutpat. Donec ut imperdiet odio. Praesent lorem est, molestie ac tincidunt vel, congue nec nibh. Etiam varius semper semper. Curabitur tempor a urna in iaculis. Pellentesque bibendum elit sit amet cursus ultrices.",
         img: '/images/colonial_shuttle.webp',
-        price: 120,
-        qty: 12
+        price: 250000,
+        qty: 8
     },
     {
         name: 'Discovery Pod',
         description: "Etiam luctus sem nec arcu accumsan, ac dignissim nisi scelerisque. Praesent eu enim ut sem euismod ornare et quis orci. Aenean risus odio, interdum at vulputate ut, tincidunt eget erat. Donec tempus tempus mollis. Integer mi metus, bibendum nec nisi vitae, fermentum accumsan dolor. Nulla faucibus quam at consectetur fermentum. Nullam mollis sem sit amet dui feugiat pulvinar. Quisque odio lectus, sodales eu scelerisque sit amet, laoreet sit amet odio. Maecenas dictum bibendum orci, non volutpat lacus consectetur at. Praesent quis sem felis. Ut vitae sem sit amet est aliquet malesuada. Aliquam fringilla in risus et molestie. Aliquam posuere dictum urna quis placerat.",
         img: '/images/discovery_pod.webp',
-        price: 120,
+        price: 112500,
         qty: 12
     },
     {
         name: 'Discovery',
         description: "Maecenas vehicula fermentum augue, sit amet ullamcorper neque blandit ac. Morbi eu ante augue. Ut est dolor, rhoncus at justo in, auctor sodales ipsum. Quisque risus quam, convallis convallis efficitur mollis, accumsan ac elit. Pellentesque vehicula pulvinar nisl, at sodales libero pretium nec. Morbi orci enim, ullamcorper in maximus at, sollicitudin vel nunc. Donec id aliquet ipsum. Nulla facilisi. Morbi convallis ut nunc nec eleifend. Vivamus in consectetur nunc. Maecenas ullamcorper pulvinar diam, non semper nisi. Vestibulum vel dignissim ligula. Sed non arcu lacus. Fusce elit felis, aliquam ac tristique at, malesuada id purus. In in pellentesque diam.",
         img: '/images/discovery.webp',
-        price: 5000,
-        qty: 6
+        price: 500000,
+        qty: 5
     },
     {
         name: 'Eagle Transporter',
         description: "Curabitur id cursus elit. Vestibulum in elementum odio. Duis cursus nibh ac diam vehicula semper. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam efficitur non eros id venenatis. Aliquam erat volutpat. Sed fermentum pharetra neque in elementum. Vivamus efficitur imperdiet nulla, non mollis elit. Maecenas sit amet auctor urna, et imperdiet sem. Maecenas vel tempor nisi. Nam eu sapien ac ex malesuada accumsan. Mauris egestas purus at massa viverra rutrum non vel dui. Aliquam facilisis mi vitae urna tempus feugiat. Ut efficitur enim consequat massa ultrices, sit amet laoreet metus molestie. Maecenas vitae lobortis purus, at rutrum ligula. Fusce sed felis nisi.",
         img: '/images/eagle_transporter.webp',
-        price: 120,
-        qty: 12
+        price: 250000,
+        qty: 8
     },
     {
         name: 'Event Horizon',
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nibh magna, pretium viverra quam a, luctus mattis nisl. Nunc vel odio mollis, dictum erat quis, placerat purus. Maecenas euismod sapien a metus porttitor, sed accumsan neque dictum. Nulla id justo elementum, luctus quam in, eleifend nisi. Nulla semper porttitor pellentesque. Mauris aliquet ex vitae sodales faucibus. Donec elementum tincidunt magna, in gravida sapien condimentum et. Vestibulum sed ultricies urna, et ornare dolor. Etiam iaculis maximus suscipit. Aliquam finibus lacinia odio. Suspendisse non lectus vulputate, molestie eros imperdiet, imperdiet ante. Integer sed hendrerit mauris, eu convallis urna. Etiam ac ultricies nisl, eu accumsan augue. Quisque vulputate, massa non dapibus fermentum, ex sem facilisis sem, quis consequat sem sapien vel lorem.",
         img: '/images/event_horizon.webp',
-        price: 120,
-        qty: 12
+        price: 3250000,
+        qty: 4
     },
     {
         name: 'Falcon',
         description: "Nunc venenatis placerat vestibulum. In ipsum nunc, sagittis vel diam sit amet, vehicula mollis tellus. Nullam turpis risus, pulvinar ut quam nec, hendrerit fringilla urna. In aliquet velit nunc, ut pulvinar arcu consequat at. Aenean vehicula dictum mauris vitae ornare. Nullam urna lectus, lobortis at suscipit ac, pretium tincidunt leo. Aliquam elementum enim eu semper dictum. Vivamus pharetra mi vel facilisis maximus. Nam tristique nunc et arcu placerat, eu varius ligula varius. Duis euismod iaculis justo. Ut porta libero eu accumsan blandit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas eu elit quam.",
         img: '/images/falcon.webp',
-        price: 120,
-        qty: 12
+        price: 775000,
+        qty: 2
     },
     {
         name: 'Fireball XL5',
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur malesuada sem et lectus laoreet sodales. Nunc gravida dui in nunc dictum, nec convallis urna tempus. In laoreet tortor ut sodales aliquet. Curabitur eleifend placerat sagittis. Maecenas a congue nibh, ac auctor massa. Cras erat tortor, ultricies nec massa ac, ullamcorper rutrum libero. Fusce a interdum magna. Pellentesque facilisis varius libero eu eleifend. Nulla at fermentum risus. Phasellus aliquet tristique sapien quis fringilla.",
         img: '/images/fireball_xl5.webp',
-        price: 120,
-        qty: 12
+        price: 94999,
+        qty: 4
     },
     {
         name: 'Gunstar',
         description: "In hac habitasse platea dictumst. Mauris pulvinar est et velit cursus, ut suscipit nisi dignissim. Curabitur ut interdum nisl, vitae finibus justo. Aenean ligula justo, tempus sit amet tempus ac, interdum nec mauris. Donec velit arcu, fermentum in feugiat eu, tincidunt in dolor. Aenean et ultricies enim. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer vitae pulvinar diam, a aliquam nunc. Proin tincidunt tellus a lacus aliquet, ut eleifend mi ultricies. Aenean euismod suscipit eros eu ullamcorper.",
         img: '/images/gunstar.webp',
-        price: 5000,
+        price: 225000,
         qty: 6
     },
     {
         name: 'Lambda Class Shuttle',
         description: "Aliquam lacinia porttitor urna, et eleifend lacus iaculis in. Praesent et nibh rutrum, ullamcorper erat at, placerat orci. Nam vitae nunc id dolor elementum fermentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nunc tincidunt porta auctor. Nulla justo elit, tincidunt vel tincidunt et, semper eget justo. Proin felis nulla, imperdiet ac iaculis sed, malesuada ut orci. Quisque cursus nisl et justo rutrum, gravida scelerisque ligula cursus. Sed non lorem a nibh consectetur tempus. Phasellus mollis, lacus non commodo condimentum, risus dui aliquam diam, at eleifend turpis erat et velit. Sed ac accumsan ipsum. Nullam vel metus non orci semper ullamcorper ut et augue. Proin interdum dictum fermentum.",
         img: '/images/lambda_class_imperial_shuttle.webp',
-        price: 120,
-        qty: 12
+        price: 199999,
+        qty: 10
     },
     {
         name: 'Liberator',
         description: 'Aliquam vel ante dolor. Praesent gravida turpis at tempor viverra. Vivamus mauris mi, lacinia nec mauris et, imperdiet ultricies turpis. Sed ac congue lectus. Pellentesque eget finibus nunc, id viverra diam. Praesent nec velit iaculis, cursus ligula eu, vulputate nibh. Sed efficitur, risus quis interdum fermentum, metus arcu vulputate metus, eu blandit urna mauris vel lacus. Maecenas ut mattis mauris. Pellentesque vitae tortor vel dolor aliquam malesuada. Integer id magna ac lectus sodales mattis vitae hendrerit tortor. Sed non sapien venenatis, molestie est sed, bibendum enim. Fusce rhoncus pulvinar eleifend. Aenean ut dolor eu nibh feugiat eleifend ullamcorper eu ex. Donec ex turpis, accumsan vitae lacinia eu, pellentesque vitae lacus. Pellentesque ut condimentum magna.',
         img: '/images/liberator.webp',
-        price: 120,
-        qty: 12
+        price: 825000,
+        qty: 3
     },
     {
         name: 'Ranger',
         description: 'Suspendisse tempor velit quis massa vehicula, ultrices venenatis mauris ultrices. Proin vestibulum venenatis nunc et venenatis. Donec feugiat, velit volutpat dapibus rutrum, sem ex facilisis neque, non iaculis felis enim et enim. Etiam ac velit consectetur urna ornare dapibus ut eu tellus. Duis ipsum diam, tincidunt quis massa et, tincidunt bibendum lorem. Nullam accumsan leo eget nulla aliquam lobortis. Nam dignissim eu elit sed feugiat. Ut blandit velit vel sem rhoncus lobortis. Sed venenatis efficitur elit non suscipit. Quisque malesuada finibus nibh, ornare luctus lorem vehicula vitae. Integer sed elementum neque, at porttitor odio. Nam mollis felis vitae metus tincidunt fermentum. Nam a iaculis neque.',
         img: '/images/ranger.webp',
-        price: 120,
+        price: 400000,
         qty: 12
     },
     {
         name: 'Reliant',
         description: 'Duis consectetur ipsum quis dignissim semper. Cras semper, sapien sit amet molestie viverra, eros diam facilisis lectus, vel hendrerit ipsum eros sed urna. Aliquam erat volutpat. Aenean id porttitor lectus, id malesuada justo. Nullam laoreet risus vitae congue aliquet. Fusce enim sapien, finibus convallis viverra eu, aliquam nec orci. Sed hendrerit tristique congue. Nunc rutrum rhoncus felis, at iaculis urna condimentum eu. Etiam euismod purus id diam varius, in luctus eros laoreet. Phasellus metus erat, condimentum laoreet odio ut, finibus faucibus quam. Donec interdum rutrum diam a viverra.',
         img: '/images/reliant.webp',
-        price: 120,
-        qty: 12
+        price: 4750000,
+        qty: 2
     },
     {
         name: 'Clipper',
         description: 'Nam faucibus ut metus vitae ornare. Praesent commodo neque quis tincidunt pharetra. Nulla tristique a dolor a efficitur. Donec odio diam, feugiat vitae ultricies at, sodales et risus. In enim urna, aliquam id aliquet nec, semper eget dolor. Etiam tempor justo elit, sit amet lacinia quam laoreet et. Integer at erat tincidunt, volutpat felis at, sodales augue. Cras cursus diam sed est suscipit suscipit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
         img: '/images/space_clipper.webp',
-        price: 5000,
-        qty: 6
+        price: 875000,
+        qty: 7
     },
     {
         name: 'Sulaco',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed enim in eros tempor condimentum vel eu nisl. Vivamus faucibus ante et tincidunt semper. Nullam dolor sapien, pharetra vitae lobortis ac, commodo vel neque. Sed ornare sodales fermentum. Nulla aliquam libero vel diam rutrum dignissim. Vivamus hendrerit justo sit amet tristique mattis. Proin sollicitudin faucibus volutpat. Phasellus pellentesque ornare justo, sit amet varius metus ultrices non.',
         img: '/images/sulaco.webp',
-        price: 120,
-        qty: 12
+        price: 3299999,
+        qty: 2       
     },
     {
         name: 'Tantive IV',
         description: 'Sed cursus ex vel urna facilisis, ac faucibus est pellentesque. Donec consectetur sed quam nec lobortis. Proin nec dictum sapien, vitae ultrices quam. Morbi tincidunt, nisi et iaculis tincidunt, eros neque mattis eros, in cursus tellus lacus id leo. Suspendisse tristique, nisi et scelerisque semper, enim dui porta magna, ut tempor risus nibh eu libero. Aenean id efficitur enim. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut sed lectus diam. Aliquam sed porttitor leo. Donec convallis, urna a convallis efficitur, sem leo pretium nunc, at ornare ante magna quis justo. Mauris et tincidunt turpis. Maecenas non laoreet diam, in laoreet sapien.',
         img: '/images/tantive_iv.webp',
-        price: 120,
-        qty: 12
+        price: 1650000,
+        qty: 8
     },
     {
         name: 'Thunderbird',
         description: 'Aenean faucibus rutrum lacus nec dignissim. Phasellus lacinia mi ac libero vestibulum, a aliquet nisi gravida. Fusce pellentesque tempor neque nec pharetra. Nullam nec tempus nisi. Vivamus mollis ultrices lorem non blandit. Cras aliquam convallis nulla in tempus. Vestibulum elit orci, porta et ante sit amet, tincidunt gravida nisl. Vestibulum pulvinar semper eros vitae pretium. Fusce lacus leo, vulputate nec mollis in, rhoncus ut sem. Mauris convallis efficitur dictum.',
         img: '/images/thunderbirds_3.webp',
-        price: 120,
-        qty: 12
+        price: 200000,
+        qty: 6
     },
     {
 
         name: 'Thunderfighter',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut blandit elit at ipsum faucibus lobortis. Nulla vehicula, mauris vel venenatis gravida, magna mauris viverra libero, ac interdum dolor enim non velit. Sed et nisl orci. In dignissim ornare nulla, vitae ornare est consequat quis. Fusce at fringilla tellus. Etiam posuere mattis ligula at iaculis. Nulla venenatis tincidunt metus quis venenatis. Donec maximus ornare elit, ut fringilla lacus. Pellentesque malesuada euismod ligula. Fusce vestibulum felis felis. Vivamus porttitor ac neque nec posuere.',
         img: '/images/thunderfighter.webp',
-        price: 120,
-        qty: 12
+        price: 249999,
+        qty: 8
     },
     {
         name: 'USCSS Starship',
         description: 'Suspendisse tempor velit quis massa vehicula, ultrices venenatis mauris ultrices. Proin vestibulum venenatis nunc et venenatis. Donec feugiat, velit volutpat dapibus rutrum, sem ex facilisis neque, non iaculis felis enim et enim. Etiam ac velit consectetur urna ornare dapibus ut eu tellus. Duis ipsum diam, tincidunt quis massa et, tincidunt bibendum lorem. Nullam accumsan leo eget nulla aliquam lobortis. Nam dignissim eu elit sed feugiat. Ut blandit velit vel sem rhoncus lobortis. Sed venenatis efficitur elit non suscipit. Quisque malesuada finibus nibh, ornare luctus lorem vehicula vitae. Integer sed elementum neque, at porttitor odio. Nam mollis felis vitae metus tincidunt fermentum. Nam a iaculis neque.',
         img: '/images/uscss_nastromo.webp',
-        price: 120,
-        qty: 12
+        price: 3750000,
+        qty: 4
     }
 ]
 
